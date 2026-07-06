80d2db7675000dfb82ba73ddc342f78b5df867ac
mintduck <yeji2335@gmail.com>
2026-07-06 16:25:01 +0900

    Add data files

---
+++ src/data/site.ts
@@ -0,0 +1,17 @@
+export const siteTitle = "부모님의 첫 복숭아 예약 주문";
+
+export const siteDescription =
+  "부모님이 직접 수확하고 하나씩 살펴 고른 복숭아를 가까운 분들께 조심스럽게 소개합니다. 필요하신 분만 편하게 예약해 주세요.";
+
+export const orderFormUrl = "https://form.naver.com/";
+
+export const heroEyebrow = "부모님의 첫 수확";
+
+export const heroTitle = "부모님이 처음 수확한 복숭아를 소개합니다.";
+
+export const heroDescription =
+  "직접 수확하고 하나씩 살펴 고른 복숭아를 가까운 분들께 조심스럽게 소개하려고 합니다. 많은 양은 아니지만 정성껏 준비했습니다.";
+
+export const primaryCTA = "예약 주문하기";
+
+export const secondaryCTA = "네이버폼으로 예약하기";
